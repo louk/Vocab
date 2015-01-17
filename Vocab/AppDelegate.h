9b964307b3fbf7b0480a4e1a3b54a2adf7d99843
Louk <louk@Louks-MacBook-Pro.local>
2015-01-17 21:58:52 +0800

    Initial Commit

---
+++ Vocab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vocab
+//
+//  Created by Louk on 1/17/15.
+//  Copyright (c) 2015 Louk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Vocab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vocab
+//
+//  Created by Louk on 1/17/15.
+//  Copyright (c) 2015 Louk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
